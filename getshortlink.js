2e204b223a462483f8c97a6de0c2b751f8d0ab1c
Lithane <plok314159265@163.com>
2018-02-19 14:53:21 +0800

    add forum slnk

--- getshortlink.js
+++ getshortlink.js
@@ -1,30 +1,56 @@
 var httpheader = /(http:\/\/|https:\/\/)?/i;
 var traillingslash = /[\/\s]*$/i;
+
+var kcregex = /ksc\.moe/i;
+
 var kcregex = /www\.kerbcat\.com\/.*\/(?=\d+)/i;
+
 var dreveregex = /dreve\.kerbcat\.com\/s\//i;
+
+var forumregex = /forum\.kerbalspaceprogram\.com\/index\.php\?\/topic\//i;
+var forumtrailling = /-[\w-]+/i;
+var forumcommentregex = /&do=findComment&comment=(?=\d+)/i;
+
 var kcsl = "http://ksc.moe/";
 
-function kcsl(inurl) {
-  if ((inurl = "")) return "";
+var getURLType = function(url) {
+  return url.match(forumregex)
+    ? 3
+    : url.match(dreveregex) ? 2 : url.match(kcregex) ? 1 : -1;
+};
 
-  url = inurl.replace(httpheader, "").replace(traillingslash, "");
+var parsekcsl = function(inurl) {
+  if (inurl == "" || inurl.match(kcregex)) return inurl;
   var result = null;
-  type = url.match(dreveregex) ? 2 : url.match(kcregex) ? 1 : -1;
-  switch (type) {
+  // type = getURLType(inurl);
+  url = inurl.replace(httpheader, "").replace(traillingslash, "");
+  switch (getURLType(inurl)) {
     case 1:
       result = "#p" + url.replace(kcregex, "");
       break;
     case 2:
       result = "#d" + url.replace(dreveregex, "");
       break;
+    case 3:
+      psurl = url
+        .replace(forumregex, "")
+        .replace(forumtrailling, "")
+        .split(forumcommentregex)
+        .join("-");
+      result = "#f" + psurl;
+      break;
+    case -1:
+      throw "NotAKerbCatWebsiteException";
+      return -1;
+      break;
   }
   if (result) {
     result = kcsl + result;
     return result;
   } else {
-    throw "Not-A-Kerbcat-Website Exception";
+    throw "InternalParseErrorException";
     return -1;
   }
-}
+};
 
-export default kscl;
+// export default kscl();
